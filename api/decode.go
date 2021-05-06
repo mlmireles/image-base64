008bd95d00bc05b3ecccc269f5db967784d4f5a6
Martín López Mireles <martinh.mireles@gmail.com>
2021-05-06 14:07:56 -0500

    Add fromBase64 method at decode

--- api/decode.go
+++ api/decode.go
@@ -1,12 +1,26 @@
 package api
 
 import (
+	"encoding/base64"
+	"encoding/json"
+	"log"
 	"net/http"
 
 	e "github.com/mlmireles/image-base64/errors"
 )
 
 func (a API) decode(w http.ResponseWriter, r *http.Request) e.HTTPError {
+	var encoded string
+	err := json.NewDecoder(r.Body).Decode(&encoded)
+	if err != nil {
+		log.Println("[encode.go] ", err)
+		return e.HTTPError{Error: e.BadRequest{}, Message: "Error reading file"}
+	}
+	log.Println(encoded)
 
 	return respond(w, nil)
+}
+
+func fromBase64(s string) ([]byte, error) {
+	return base64.StdEncoding.DecodeString(s)
 }
